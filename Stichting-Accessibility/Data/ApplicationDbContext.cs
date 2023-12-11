d699b549ee3baba145cf1813f90a44c5bf6d43cf
Faaruz <faaruz.uddin@gmail.com>
2023-12-11 11:31:16 +0100

    template

---
+++ Stichting-Accessibility/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using Stichting_Accessibility.Models;
+
+namespace Stichting_Accessibility.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+    }
+}
---
+++ Stichting-Accessibility/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Stichting_Accessibility.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
