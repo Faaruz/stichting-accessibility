3d4d3ba1cef21dcbf0444b44e5efe1276b6f2651
Yannick van Vulpen <yannick.graal@gmail.com>
2023-12-20 11:38:34 +0100

    Ervaringsdeskundige registratie versimpelt, vragen over beperking worden later tijdens onboarding gesteld.

--- Stichting-Accessibility/ClientApp/src/components/pages/ErvaringsdeskundigeRegistration.js
+++ Stichting-Accessibility/ClientApp/src/components/pages/ErvaringsdeskundigeRegistration.js
@@ -1,9 +1,8 @@
-﻿// ErvaringsdeskundigeRegistration.js
-import React, { useState } from 'react';
+﻿import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const ErvaringsdeskundigeRegistration = () => {
-    // State to track form values and step
+    // State to track form values
     const [formValues, setFormValues] = useState({
         voornaam: '',
         achternaam: '',
@@ -14,8 +13,10 @@ const ErvaringsdeskundigeRegistration = () => {
         typeBeperking: '',
         hulpmiddelen: '',
         ziektes: '',
+        gebruikersnaam: '',
+        wachtwoord: '',
+        bevestigWachtwoord: '',
     });
-    const [step, setStep] = useState(1);
 
     // Function to handle form field changes
     const handleInputChange = (e) => {
@@ -26,131 +27,72 @@ const ErvaringsdeskundigeRegistration = () => {
         });
     };
 
-    // Function to handle form submission and navigate to the next step
-    const handleNextStep = (e) => {
+    // Function to handle form submission
+    const handleFormSubmit = (e) => {
         e.preventDefault();
-        setStep(step + 1);
+        // Add any additional logic needed for form submission
+        console.log('Form submitted:', formValues);
     };
 
     return (
-            <div className="ervaringsdeskundige-registration-page">
-                <h2>Ervaringsdeskundige Registratie</h2>
+        <div className="ervaringsdeskundige-registration-page">
+            <h2>Ervaringsdeskundige Registratie</h2>
 
-                {step === 1 && (
-                    <Form onSubmit={handleNextStep}>
-                        {/* ... form fields for step 1 */}
-                        <FormGroup>
-                            <Label for="voornaam">Voornaam</Label>
-                            <Input type="text" name="voornaam" id="voornaam" value={formValues.voornaam} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="achternaam">Achternaam</Label>
-                            <Input type="text" name="achternaam" id="achternaam" value={formValues.achternaam} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="postcode">Postcode</Label>
-                            <Input type="text" name="postcode" id="postcode" value={formValues.postcode} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="woonplaats">Woonplaats</Label>
-                            <Input type="text" name="woonplaats" id="woonplaats" value={formValues.woonplaats} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="email">E-mail</Label>
-                            <Input type="email" name="email" id="email" value={formValues.email} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="telefoonnummer">Telefoonnummer</Label>
-                            <Input type="tel" name="telefoonnummer" id="telefoonnummer" value={formValues.telefoonnummer} onChange={handleInputChange} />
-                        </FormGroup>
-                        <div className="continue-button">
-                            <Button color="primary" size="lg" type="submit">
-                                Ga verder naar Stap 2
-                            </Button>
-                        </div>
-                    </Form>
-                )}
-
-                {step === 2 && (
-                    <Form>
-                        {/* ... form fields for step 2 */}
-                        <FormGroup>
-                            <Label for="typeBeperking">Type beperking</Label>
-                            <Input
-                                type="select"
-                                name="typeBeperking"
-                                id="typeBeperking"
-                                value={formValues.typeBeperking}
-                                onChange={handleInputChange}
-                            >
-                                <option value="">Selecteer...</option>
-                                <option value="fysiek">Fysieke beperking</option>
-                                <option value="visueel">Visuele beperking</option>
-                                <option value="visueel">Mentale beperking</option>
-                                {/* Add more options as needed */}
-                            </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="hulpmiddelen">Eventuele Hulpmiddelen</Label>
-                            <Input
-                                type="select"
-                                name="hulpmiddelen"
-                                id="hulpmiddelen"
-                                value={formValues.hulpmiddelen}
-                                onChange={handleInputChange}
-                            >
-                                <option value="">Selecteer...</option>
-                                <option value="braille">Braille</option>
-                                <option value="texttospeach">Voorlees Software</option>
-                                <option value="geleidehond">Geleidehond</option>
-                                <option value="Geen Hulpmiddelen">Geen Hulpmiddel</option>
-                                {/* Add more options as needed */}
-                            </Input>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="ziektes">Eventuele ziektes</Label>
-                            <Input type="text" name="ziektes" id="ziektes" value={formValues.ziektes} onChange={handleInputChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="gebruikersnaam">Gebruikersnaam</Label>
-                            <Input
-                                type="text"
-                                name="gebruikersnaam"
-                                id="gebruikersnaam"
-                                value={formValues.gebruikersnaam}
-                                onChange={handleInputChange}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="wachtwoord">Wachtwoord</Label>
-                            <Input
-                                type="password"
-                                name="wachtwoord"
-                                id="wachtwoord"
-                                value={formValues.wachtwoord}
-                                onChange={handleInputChange}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="bevestigWachtwoord">Bevestig Wachtwoord</Label>
-                            <Input
-                                type="password"
-                                name="bevestigWachtwoord"
-                                id="bevestigWachtwoord"
-                                value={formValues.bevestigWachtwoord}
-                                onChange={handleInputChange}
-                            />
-                        </FormGroup>
-                        <div className="continue-button">
-                            <Button color="primary" size="lg" type="submit">
-                                Voltooien
-                            </Button>
-                        </div>
-                    </Form>
-                )}
-            </div>
+            <Form onSubmit={handleFormSubmit}>
+                <FormGroup>
+                    <Label for="voornaam">Voornaam</Label>
+                    <Input type="text" name="voornaam" id="voornaam" value={formValues.voornaam} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="achternaam">Achternaam</Label>
+                    <Input type="text" name="achternaam" id="achternaam" value={formValues.achternaam} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="postcode">Postcode</Label>
+                    <Input type="text" name="postcode" id="postcode" value={formValues.postcode} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="woonplaats">Woonplaats</Label>
+                    <Input type="text" name="woonplaats" id="woonplaats" value={formValues.woonplaats} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="email">E-mail</Label>
+                    <Input type="email" name="email" id="email" value={formValues.email} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="telefoonnummer">Telefoonnummer</Label>
+                    <Input type="tel" name="telefoonnummer" id="telefoonnummer" value={formValues.telefoonnummer} onChange={handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="wachtwoord">Wachtwoord</Label>
+                    <Input
+                        type="password"
+                        name="wachtwoord"
+                        id="wachtwoord"
+                        value={formValues.wachtwoord}
+                        onChange={handleInputChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="bevestigWachtwoord">Bevestig Wachtwoord</Label>
+                    <Input
+                        type="password"
+                        name="bevestigWachtwoord"
+                        id="bevestigWachtwoord"
+                        value={formValues.bevestigWachtwoord}
+                        onChange={handleInputChange}
+                    />
+                </FormGroup>
+                <div className="continue-button">
+                    <Button color="primary" size="lg" type="submit">
+                        Voltooien
+                    </Button>
+                </div>
+            </Form>
+        </div>
     );
 };
 
 export default ErvaringsdeskundigeRegistration;
+
 
