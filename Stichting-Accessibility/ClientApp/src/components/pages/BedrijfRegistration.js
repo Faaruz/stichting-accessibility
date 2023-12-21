3e2d8d06006d9d340ce9a8605a86ec7da5ec1187
yannick <yannick.graal@gmail.com>
2023-12-21 12:41:35 +0100

    Form validation code added to bedrijf registration based on existing form validation code in ervaringsdeskundige registration.

--- Stichting-Accessibility/ClientApp/src/components/pages/BedrijfRegistration.js
+++ Stichting-Accessibility/ClientApp/src/components/pages/BedrijfRegistration.js
@@ -15,6 +15,7 @@ const BedrijfRegistration = () => {
     });
 
     const [step, setStep] = useState(1);
+    const [errors, setErrors] = useState({});
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -22,16 +23,94 @@ const BedrijfRegistration = () => {
             ...prevValues,
             [name]: value,
         }));
+
+        // Validate input based on the field name
+        validateInput(name, value);
+    };
+
+    const validateInput = (name, value) => {
+        const newErrors = { ...errors };
+
+        // Check if voornaam and achternaam only contain letters (including spaces)
+        const nameRegex = /^[a-zA-Z\s]+$/;
+        if ((name === 'voornaam' || name === 'achternaam') && !nameRegex.test(value)) {
+            newErrors[name] = `${name.charAt(0).toUpperCase() + name.slice(1)} moet alleen letters en spaties bevatten.`;
+        } else {
+            delete newErrors[name];
+        }
+
+        // Check if telefoonnummer only contains numbers
+        const phoneRegex = /^\d+$/;
+        if (name === 'telefoonnummer' && !phoneRegex.test(value)) {
+            newErrors.telefoonnummer = 'Telefoonnummer moet alleen cijfers bevatten.';
+        } else {
+            delete newErrors.telefoonnummer;
+        }
+
+        // Check if postcode has the format of 4 numbers and 2 letters
+        const postcodeRegex = /^\d{4}[A-Za-z]{2}$/;
+        if (name === 'postcode' && !postcodeRegex.test(value)) {
+            newErrors.postcode = 'Ongeldige postcode-indeling.';
+        } else {
+            delete newErrors.postcode;
+        }
+        
+        // Check if plaats only contains letters (including spaces)
+        const plaatsRegex = /^[a-zA-Z\s]+$/;
+        if (name === 'plaats' && !plaatsRegex.test(value)) {
+            newErrors.plaats = 'Plaats moet alleen letters en spaties bevatten.';
+        } else {
+            delete newErrors.plaats;
+        }
+
+        // Check if wachtwoord meets the specified criteria
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})$/;
+        if (name === 'wachtwoord' && !passwordRegex.test(value)) {
+            newErrors.wachtwoord =
+                'Wachtwoord moet minstens 8 tekens lang zijn en een hoofdletter en een leesteken (!@#$%^&*) bevatten.';
+        } else {
+            delete newErrors.wachtwoord;
+        }
+
+        // Check if wachtwoord and bevestigWachtwoord match
+        if (name === 'bevestigWachtwoord' && value !== formValues.wachtwoord) {
+            newErrors.bevestigWachtwoord = 'Wachtwoord en bevestig wachtwoord moeten overeenkomen.';
+        } else {
+            delete newErrors.bevestigWachtwoord;
+        }
+
+        setErrors(newErrors);
     };
 
     const handleNextStep = (e) => {
         e.preventDefault();
-        setStep((prevStep) => prevStep + 1);
+        // Validate all fields before proceeding to the next step
+        const isStepValid = Object.keys(errors).length === 0;
+        if (isStepValid) {
+            setStep((prevStep) => prevStep + 1);
+        }
     };
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log('Form submitted:', formValues);
+        // Validate all fields before submitting the form
+        const isFormValid = Object.keys(errors).length === 0;
+
+        // Additional validation for password criteria
+        const passwordCriteriaMet =
+            formValues.wachtwoord &&
+            /^(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})$/.test(formValues.wachtwoord);
+
+        if (isFormValid && passwordCriteriaMet) {
+            // Add any additional logic needed for form submission
+            console.log('Form submitted:', formValues);
+        } else {
+            // Password mismatch error message
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                bevestigWachtwoord: 'Wachtwoord en bevestig wachtwoord moeten overeenkomen, teminste 8 tekens lang zijn en een hoofdletter en leesteken bevatten.',
+            }));
+        }
     };
 
     return (
@@ -50,6 +129,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.voornaam}
                                 onChange={handleInputChange}
                             />
+                            {errors.voornaam && (
+                                <span className="error-message">{errors.voornaam}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="achternaam">Achternaam contactpersoon</Label>
@@ -61,6 +143,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.achternaam}
                                 onChange={handleInputChange}
                             />
+                            {errors.achternaam && (
+                                <span className="error-message">{errors.achternaam}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="bedrijfsnaam">Bedrijfsnaam</Label>
@@ -72,6 +157,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.bedrijfsnaam}
                                 onChange={handleInputChange}
                             />
+                            {errors.bedrijfsnaam && (
+                                <span className="error-message">{errors.bedrijfsnaam}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="email">Email</Label>
@@ -83,6 +171,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.email}
                                 onChange={handleInputChange}
                             />
+                            {errors.email && (
+                                <span className="error-message">{errors.email}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="telefoonnummer">Telefoonnummer</Label>
@@ -94,6 +185,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.telefoonnummer}
                                 onChange={handleInputChange}
                             />
+                            {errors.telefoonnummer && (
+                                <span className="error-message">{errors.telefoonnummer}</span>
+                            )}
                         </FormGroup>
                         <div className="continue-button">
                             <Button color="primary" size="lg" type="submit">
@@ -117,6 +211,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.postcode}
                                 onChange={handleInputChange}
                             />
+                            {errors.postcode && (
+                                <span className="error-message">{errors.postcode}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="plaats">Plaats</Label>
@@ -128,6 +225,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.plaats}
                                 onChange={handleInputChange}
                             />
+                            {errors.plaats && (
+                                <span className="error-message">{errors.plaats}</span>
+                            )}
                         </FormGroup>
                         <p>Login met Google, Microsoft of handmatig</p>
                         <FormGroup>
@@ -140,6 +240,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.wachtwoord}
                                 onChange={handleInputChange}
                             />
+                            {errors.wachtwoord && (
+                                <span className="error-message">{errors.wachtwoord}</span>
+                            )}
                         </FormGroup>
                         <FormGroup>
                             <Label for="bevestigWachtwoord">Bevestig Wachtwoord</Label>
@@ -151,6 +254,9 @@ const BedrijfRegistration = () => {
                                 value={formValues.bevestigWachtwoord}
                                 onChange={handleInputChange}
                             />
+                            {errors.bevestigWachtwoord && (
+                                <span className="error-message">{errors.bevestigWachtwoord}</span>
+                            )}
                         </FormGroup>
                         <div className="continue-button">
                             <Button color="primary" size="lg" type="submit">
