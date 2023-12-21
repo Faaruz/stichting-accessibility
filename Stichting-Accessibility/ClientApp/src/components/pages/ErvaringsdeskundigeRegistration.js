58a15340ac8ace508d0b90c246159bcd56608604
yannick <yannick.graal@gmail.com>
2023-12-21 12:22:46 +0100

    Form validation for Ervaringsdeskundige registration as well as CSS changes to better accomodate screenreaders.

--- Stichting-Accessibility/ClientApp/src/components/pages/ErvaringsdeskundigeRegistration.js
+++ Stichting-Accessibility/ClientApp/src/components/pages/ErvaringsdeskundigeRegistration.js
@@ -14,6 +14,7 @@ const ErvaringsdeskundigeRegistration = () => {
     });
 
     const [step, setStep] = useState(1);
+    const [errors, setErrors] = useState({});
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -21,17 +22,93 @@ const ErvaringsdeskundigeRegistration = () => {
             ...prevValues,
             [name]: value,
         }));
+
+        // Validate input based on the field name
+        validateInput(name, value);
+    };
+
+    const validateInput = (name, value) => {
+        const newErrors = { ...errors };
+
+        // Check if voornaam and achternaam only contain letters (including spaces)
+        const nameRegex = /^[a-zA-Z\s]+$/;
+        if ((name === 'voornaam' || name === 'achternaam') && !nameRegex.test(value)) {
+            newErrors[name] = `${name.charAt(0).toUpperCase() + name.slice(1)} moet alleen letters en spaties bevatten.`;
+        } else {
+            delete newErrors[name];
+        }
+
+        // Check if postcode has the format of 4 numbers and 2 letters
+        const postcodeRegex = /^\d{4}[A-Za-z]{2}$/;
+        if (name === 'postcode' && !postcodeRegex.test(value)) {
+            newErrors.postcode = 'Ongeldige postcode-indeling.';
+        } else {
+            delete newErrors.postcode;
+        }
+
+        // Check if woonplaats only contains letters (including spaces)
+        if (name === 'woonplaats' && !nameRegex.test(value)) {
+            newErrors.woonplaats = 'Woonplaats moet alleen letters en spaties bevatten.';
+        } else {
+            delete newErrors.woonplaats;
+        }
+
+        // Check if telefoonnummer only contains numbers
+        const phoneRegex = /^\d+$/;
+        if (name === 'telefoonnummer' && !phoneRegex.test(value)) {
+            newErrors.telefoonnummer = 'Telefoonnummer moet alleen cijfers bevatten.';
+        } else {
+            delete newErrors.telefoonnummer;
+        }
+
+        // Check if wachtwoord meets the specified criteria
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})$/;
+        if (name === 'wachtwoord' && !passwordRegex.test(value)) {
+            newErrors.wachtwoord =
+                'Wachtwoord moet minstens 8 tekens lang zijn en een hoofdletter en een leesteken (!@#$%^&*) bevatten.';
+        } else {
+            delete newErrors.wachtwoord;
+        }
+
+        // Check if wachtwoord and bevestigWachtwoord match
+        if (name === 'bevestigWachtwoord' && value !== formValues.wachtwoord) {
+            newErrors.bevestigWachtwoord = 'Wachtwoord en bevestig wachtwoord moeten overeenkomen.';
+        } else {
+            delete newErrors.bevestigWachtwoord;
+        }
+
+        setErrors(newErrors);
     };
 
     const handleNextStep = (e) => {
         e.preventDefault();
-        setStep((prevStep) => prevStep + 1);
+        // Validate all fields before proceeding to the next step
+        const isStepValid = Object.keys(errors).length === 0;
+        if (isStepValid) {
+            setStep((prevStep) => prevStep + 1);
+        }
     };
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        // Add any additional logic needed for form submission
-        console.log('Form submitted:', formValues);
+        // Validate all fields before submitting the form
+        const isFormValid = Object.keys(errors).length === 0;
+
+        // Additional validation for password criteria
+        const passwordCriteriaMet =
+            formValues.wachtwoord &&
+            /^(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})$/.test(formValues.wachtwoord);
+
+        if (isFormValid && passwordCriteriaMet) {
+            // Add any additional logic needed for form submission
+            console.log('Form submitted:', formValues);
+        } else {
+            // Password mismatch error message
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                bevestigWachtwoord: 'Wachtwoord en bevestig wachtwoord moeten overeenkomen, teminste 8 tekens lang zijn en een hoofdletter en leesteken bevatten.',
+            }));
+        }
     };
 
     return (
@@ -42,19 +119,59 @@ const ErvaringsdeskundigeRegistration = () => {
                 <Form onSubmit={handleNextStep}>
                     <FormGroup>
                         <Label for="voornaam">Voornaam</Label>
-                        <Input type="text" name="voornaam" required={true} id="voornaam" value={formValues.voornaam} onChange={handleInputChange} />
+                        <Input
+                            type="text"
+                            name="voornaam"
+                            required={true}
+                            id="voornaam"
+                            value={formValues.voornaam}
+                            onChange={handleInputChange}
+                        />
+                        {errors.voornaam && (
+                            <span className="error-message">{errors.voornaam}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="achternaam">Achternaam</Label>
-                        <Input type="text" name="achternaam" required={true} id="achternaam" value={formValues.achternaam} onChange={handleInputChange} />
+                        <Input
+                            type="text"
+                            name="achternaam"
+                            required={true}
+                            id="achternaam"
+                            value={formValues.achternaam}
+                            onChange={handleInputChange}
+                        />
+                        {errors.achternaam && (
+                            <span className="error-message">{errors.achternaam}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="postcode">Postcode</Label>
-                        <Input type="text" name="postcode" required={true} id="postcode" value={formValues.postcode} onChange={handleInputChange} />
+                        <Input
+                            type="text"
+                            name="postcode"
+                            required={true}
+                            id="postcode"
+                            value={formValues.postcode}
+                            onChange={handleInputChange}
+                        />
+                        {errors.postcode && (
+                            <span className="error-message">{errors.postcode}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="woonplaats">Woonplaats</Label>
-                        <Input type="text" name="woonplaats" required={true} id="woonplaats" value={formValues.woonplaats} onChange={handleInputChange} />
+                        <Input
+                            type="text"
+                            name="woonplaats"
+                            required={true}
+                            id="woonplaats"
+                            value={formValues.woonplaats}
+                            onChange={handleInputChange}
+                        />
+                        {errors.woonplaats && (
+                            <span className="error-message">{errors.woonplaats}</span>
+                        )}
                     </FormGroup>
                     <div className="continue-button">
                         <Button color="primary" size="lg" type="submit">
@@ -68,11 +185,31 @@ const ErvaringsdeskundigeRegistration = () => {
                 <Form onSubmit={handleFormSubmit}>
                     <FormGroup>
                         <Label for="email">E-mail</Label>
-                        <Input type="email" name="email" required={true} id="email" value={formValues.email} onChange={handleInputChange} />
+                        <Input
+                            type="email"
+                            name="email"
+                            required={true}
+                            id="email"
+                            value={formValues.email}
+                            onChange={handleInputChange}
+                        />
+                        {errors.email && (
+                            <span className="error-message">{errors.email}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="telefoonnummer">Telefoonnummer</Label>
-                        <Input type="tel" name="telefoonnummer" required={true} id="telefoonnummer" value={formValues.telefoonnummer} onChange={handleInputChange} />
+                        <Input
+                            type="tel"
+                            name="telefoonnummer"
+                            required={true}
+                            id="telefoonnummer"
+                            value={formValues.telefoonnummer}
+                            onChange={handleInputChange}
+                        />
+                        {errors.telefoonnummer && (
+                            <span className="error-message">{errors.telefoonnummer}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="wachtwoord">Wachtwoord</Label>
@@ -84,6 +221,9 @@ const ErvaringsdeskundigeRegistration = () => {
                             value={formValues.wachtwoord}
                             onChange={handleInputChange}
                         />
+                        {errors.wachtwoord && (
+                            <span className="error-message">{errors.wachtwoord}</span>
+                        )}
                     </FormGroup>
                     <FormGroup>
                         <Label for="bevestigWachtwoord">Bevestig Wachtwoord</Label>
@@ -95,6 +235,9 @@ const ErvaringsdeskundigeRegistration = () => {
                             value={formValues.bevestigWachtwoord}
                             onChange={handleInputChange}
                         />
+                        {errors.bevestigWachtwoord && (
+                            <span className="error-message">{errors.bevestigWachtwoord}</span>
+                        )}
                     </FormGroup>
                     <div className="continue-button">
                         <Button color="primary" size="lg" type="submit">
@@ -106,5 +249,4 @@ const ErvaringsdeskundigeRegistration = () => {
         </div>
     );
 };
-
 export default ErvaringsdeskundigeRegistration;
