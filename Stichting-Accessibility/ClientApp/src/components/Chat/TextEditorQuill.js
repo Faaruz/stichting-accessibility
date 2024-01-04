d7ddc74bbe6a1c0f3ce8ff3b01ba3a5448fcc9b1
Yannick van Vulpen <yannick.graal@gmail.com>
2024-01-04 11:50:28 +0100

    Progress on fixing the quill whitespace bug. Now fixed when using bold, italics and other text modifications. Still broken in default text style.

---
+++ Stichting-Accessibility/ClientApp/src/components/Chat/TextEditorQuill.js
@@ -0,0 +1,14 @@
+﻿import React, {useState} from "react";
+import ReactQuill from "react-quill";
+
+const TextEditorQuill = props => {
+    const [value, setValue] = useState(props.value);
+
+    return (
+        <ReactQuill
+            onChange={setValue}
+            onBlur={() => props.onChange(value)}
+        />
+    );
+}
+export default ReactQuill;
